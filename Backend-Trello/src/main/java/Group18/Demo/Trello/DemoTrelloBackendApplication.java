bc4b4dd18e7c3d604706964c0b19af48bac8fa7d
Muhammad Shafay Zulfiqar <mh278164@dal.ca>
2024-01-03 14:18:33 -0400

    files added

---
+++ Backend-Trello/src/main/java/Group18/Demo/Trello/DemoTrelloBackendApplication.java
@@ -0,0 +1,13 @@
+package Group18.Demo.Trello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTrelloBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTrelloBackendApplication.class, args);
+	}
+
+}
---
+++ Backend-Trello/src/test/java/Group18/Demo/Trello/DemoTrelloBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package Group18.Demo.Trello;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTrelloBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
